b05fdccce34cf911a09d94c26853b744f00c7039
prateek <stupidcoder22@gmail.com>
2022-04-30 21:10:40 +0530

    ui done

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = document.querySelector(".headline");
+heading.innerHTML = "<h1>Inner html has been changed bro</h1>";
+heading.innerHTML = heading.innerHTML + '<button class="btn">Add More</button>';
