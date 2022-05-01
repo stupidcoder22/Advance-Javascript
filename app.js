110146373258ef8b1b3a9043bc45ed971e85020b
prateek <stupidcoder22@gmail.com>
2022-05-01 10:46:39 +0530

    done

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const heading = document.querySelector(".headline");
-heading.innerHTML = "<h1>Inner html has been changed bro</h1>";
-heading.innerHTML = heading.innerHTML + '<button class="btn">Add More</button>';
+const form = document.querySelector(".form");
+const inputText = document.getElementById("inputText");
+const todolist = document.querySelector(".todolist");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const textval = inputText.value;
+  inputText.value = "";
+  const li = document.createElement("li");
+  li.innerHTML = `
+        <span class="text">${textval}</span>
+        <div class="todolistbtn">
+            <button class="todobtn done">Done</button>
+            <button class="todobtn remove">Remove</button>
+        </div>
+        `;
+  todolist.append(li);
+});
+
+todolist.addEventListener("click", (e) => {
+  //   console.log(e.target.classList);
+  if (e.target.classList.contains("done")) {
+    const spantarget = e.target.parentNode.previousElementSibling;
+    spantarget.style.textDecoration = "line-through";
+  }
+  if (e.target.classList.contains("remove")) {
+    const targetli = e.target.parentNode.parentNode;
+    targetli.remove();
+  }
+});
